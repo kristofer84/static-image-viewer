ec124c3de636849512cde6fc0daee056ca7b9307
Kristofer Nilsson <kristofer.nilsson@tui.se>
2025-05-12 22:31:05 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+
+
 export default defineConfig({
-  base: '/',
+  base: '/static-image-viewer/',
   plugins: [vue()],
   build: {
     outDir: 'dist',
